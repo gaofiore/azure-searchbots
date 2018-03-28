ff619a1f3034a0d80157d7897129cc987879fc05
Gabriel Fiorenzano <gfiorenzano@CalgaryChamber.com>
2018-03-28 12:05:54 -0600

    Base bot - only search org, not connected yet

--- dialogs/musicianSearch.js
+++ dialogs/musicianSearch.js
@@ -1,24 +1,32 @@
-module.exports = function () {
-    bot.dialog('/musicianSearch', [
-        function (session) {
-            //Prompt for string input
-            builder.Prompts.text(session, "Type in the name of the musician you are searching for:");
-        },
-        function (session, results) {
-            //Sets name equal to resulting input
-            var name = results.response;
+const builder = require('botbuilder');
+const searchHelper = require('../searchHelpers.js');
+const messageHelper = require('../messageHelper.js');
 
-            var queryString = searchQueryStringBuilder('search= ' + name);
-            performSearchQuery(queryString, function (err, result) {
+module.exports = {
+    id: 'musicianSearch',
+    title: 'Search Organizations',
+    dialog: [
+        (session) => {
+            //Prompt for string input
+            builder.Prompts.text(session, 'Who are you searching for?');
+        },
+        (session, results) => {
+            //Sets name equal to resulting input
+            const keyword = results.response;
+
+            searchHelper.searchQuery(keyword, (err, result) => {
                 if (err) {
-                    console.log("Error when searching for musician: " + err);
-                } else if (result && result['value'] && result['value'][0]) {
-                    //If we have results send them to the showResults dialog (acts like a decoupled view)
-                    session.replaceDialog('/showResults', { result });
+                    console.log(`Search query failed with ${err}`);
+                    session.endConversation(`Sorry, I had an error when talking to the server.`);
+                } else if (result && result.length > 0) {
+                    const message = messageHelper.getMusiciansCarousel(session, result);
+                    session.endConversation(message);
                 } else {
-                    session.endDialog("No musicians by the name \'" + name + "\' found");
+                    const message = "I couldn't find any organizations by that name";
+                    session.endConversation(message);
                 }
-            })
+                session.reset('/');
+            });
         }
-    ]);
+    ]
 }
--- searchHelpers.js
+++ searchHelpers.js
@@ -1,18 +1,66 @@
-module.exports = function () {
-    global.request = require('request');
+const request = require('request');
+const rootQueryString = `https://${process.env.AZURE_SEARCH_NAME}.search.windows.net/indexes/${process.env.AZURE_INDEX_NAME}/docs?api-version=2016-09-01`;
 
-    global.searchQueryStringBuilder = function (query) {
-        return queryString + query;
-    }
-
-    global.performSearchQuery = function (queryString, callback) {
-        request(queryString, function (error, response, body) {
-            if (!error && response && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                callback(null, result);
-            } else {
-                callback(error, null);
+module.exports = {
+    facetQuery: (facet, callback) => {
+        const options = {
+            url: `${rootQueryString}&facet=${facet}`,
+            headers: {
+                'api-key': `${process.env.AZURE_SEARCH_KEY}`
             }
-        })
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (!result || !result['@search.facets'] || !result['@search.facets'][facet]) {
+                    // No items for that facet found
+                    callback(null, null);
+                } else {
+                    callback(null, result['@search.facets'][facet]);
+                }
+            }
+        });
+    },
+    filterQuery: (filterName, filterValue, callback) => {
+        const options = {
+            url: `${rootQueryString}&$filter=${filterName} eq '${filterValue}'`,
+            headers: {
+                'api-key': `${process.env.AZURE_SEARCH_KEY}`
+            }
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (result.value && result.value.length > 0) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
+    },
+    searchQuery: (keyword, callback) => {
+        const options = {
+            url: `${rootQueryString}&search=${keyword}`,
+            headers: {
+                'api-key': `${process.env.AZURE_SEARCH_KEY}`
+            }
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (result && result.value) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
     }
 }
